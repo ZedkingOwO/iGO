59cd74034373e7159b78cd18eccda0d2d4505524
lxj <505697096@qq.com>
2024-05-14 22:41:38 +0800

    C

--- projects/test4/main.go
+++ projects/test4/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var s = 27979 // 6 x 16^3 + 13x16^2 + 4x16^1 + 11 =
+	fmt.Printf("%+v %[1]d %[1]b %[1]o  %[1]x %[1]X %[1]c %[1]U\n", s)
+	t := fmt.Sprintf("%+v %[1]d %[1]b %[1]o  %[1]x %[1]X %[1]c %[1]U\n", s)
+	//fmt.print(s) 缺省
+	fmt.Println(t)
+	// 生产字符串，然后输出到标准输出
 }
